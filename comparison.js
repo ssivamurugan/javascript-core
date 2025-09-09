1d4d1754dec70993e989ce689167aa961e363144
ssivamurugan <sivamurugan492@gmail.com>
2025-09-09 12:49:44 +0530

    Comparison's added

---
+++ comparison.js
@@ -0,0 +1,27 @@
+// Number and String comparison
+
+console.log( 1 == '1'); // '1' converted into 1
+console.log( 1 === '1');
+
+// Number and Boolean comparison
+
+console.log( 1 == true); // Number(true) -> 1
+
+// String and Boolean comparison
+
+// Number('true') -> NaN
+// Number(true) -> 1
+console.log( true == 'true'); 
+
+// Null and Undefined
+
+console.log( null == undefined);
+
+// Null and zero
+
+console.log( null == 0);
+
+// Undefined and zero
+
+console.log( undefined == 0);
+
